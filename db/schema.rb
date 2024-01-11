6dddb6db949dd9961c74408bbff65d1474171558
RaviShankar2308-su <ravi.singhalcs2018@indoreinstitute.com>
2024-01-11 10:26:53 -0600

    Add Post Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_11_162340) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_11_162619) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username", null: false
     t.string "password", null: false
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_11_162340) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
